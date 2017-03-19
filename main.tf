cf01433799996a9ec984d8d6c337d4ea9c3dbc4b
Michal Charemza <michal@charemza.name>
2017-03-19 19:56:13 +0000

    No encrypted state, hhttpserver_infrastructure user

--- main.tf
+++ main.tf
@@ -1,15 +1,20 @@
 ################################################################################
 ## Common
 
+# Repeated in backend config
+variable "state_bucket" {
+  type = "string"
+  default = "terraform.charemza.name"
+}
+
 terraform {
   backend "s3" {
+    # Repeated in variables above, since can't use interpolation here
     bucket = "terraform.charemza.name"
     key    = "infrastructure.tfstate"
     region = "eu-west-1"
     profile = "infrastructure"
     lock_table = "infrastructure"
-    encrypt = "1"
-    kms_key_id = "arn:aws:kms:eu-west-1:772663561820:key/a088d01e-8d92-4766-ad98-ad8aebacdebd"
   }
 }
 
@@ -28,6 +33,29 @@ resource "aws_iam_user" "hhttpserver_infrastructure" {
 resource "aws_iam_access_key" "hhttpserver_infrastructure_key" {
   user = "${aws_iam_user.hhttpserver_infrastructure.name}"
   pgp_key = "${file("public.key")}"
+}
+
+resource "aws_iam_user_policy_attachment" "hhttpserver_infrastructure" {
+  user = "${aws_iam_user.hhttpserver_infrastructure.name}"
+  policy_arn = "${aws_iam_policy.hhttpserver_infrastructure.arn}"
+}
+
+resource "aws_iam_policy" "hhttpserver_infrastructure" {
+  name = "hhttpserver_infrastructure"
+  policy = "${data.aws_iam_policy_document.hhttpserver_infrastructure.json}"
+}
+
+data "aws_iam_policy_document" "hhttpserver_infrastructure" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:Put*",
+      "s3:Get*"
+    ]
+    resources = [
+      "arn:aws:s3:::${var.state_bucket}/hhttpserver.tfstate"
+    ]
+  }
 }
 
 output "hhttpserver_infrastructure_aws_access_key_id" {
